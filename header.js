99389aecde2bc708e16d6f898de1c0d1b537276a
Luisa Fernanda Cruz <luisafernandaclases@gmail.com>
2024-08-19 20:28:33 -0500

    Integración del diseño responsive en el header

--- header.js
+++ header.js
@@ -1,5 +1,10 @@
 fetch('./header.html')
-    .then(response => response.text())
-    .then(data => {
-            document.getElementById('header').innerHTML = data
-    })
+	.then(response => response.text())
+	.then(data => {
+		document.getElementById('header').innerHTML = data
+
+		document.querySelector('.menu-toggle').addEventListener('click', ()=>{
+		document.querySelector('nav ul').classList.toggle('active')
+	})
+}).catch(error => console.error('Error al cargar el header:', error))
+
